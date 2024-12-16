2f4a5c69a233cb919e60c4ef6b4be022c9f39b96
luis Colina <colina.1990lz@gmail.com>
2024-12-16 16:02:40 -0400

    devices section with marquees

--- src/components/Devices.tsx
+++ src/components/Devices.tsx
@@ -1,9 +1,20 @@
-
-
+import {data, data2} from "../utils/data.tsx";
+import style from "./styles/devices.module.css"
 const Devices = () => {
+    console.log(data)
     return (
-        <div>
-devices
+        <div className={style.container}>
+            <section className={style.devices}>
+                <h4 className={style.header}>Watch IBO Player with these compatible streaming devices</h4>
+                <div className={style.marquee}>
+                    <div className={style.marquee__content}>
+                        {data.map(element => <img src={element.image} alt={element.name} key={element.name}/>)}
+                    </div>
+                    <div className={style.marquee__content} aria-hidden={true}>
+                        {data2.map(element => <img src={element.image} alt={element.name} key={element.name}/>)}
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
