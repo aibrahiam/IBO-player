da27b555ea074a464d0e0a0f8403e1418cc1846e
luis Colina <colina.1990lz@gmail.com>
2024-12-17 15:37:57 -0400

    half of the features container created

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,13 +1,32 @@
 import Partners from "./Partners.tsx";
-
+import style from "./styles/features.module.css"
 
 const Features = () => {
     return (
-        <div>
-            Features
-            <Partners />
+        <section>
+            <div className={style.header}>
+                <div>Our Features</div>
+                <h4>Seamless Streaming Across Every Screen</h4>
+                <p>From your TV to your tablet or smartphone, our platform ensures a consistent and immersive viewing
+                    experience.</p>
+            </div>
+            <div className={style.grid}>
+                <div className={style.a}>
+                    <img src="/assets/1-DXXIehDi.png" alt="placeholder"/>
+                </div>
+                <div className={style.b}>
+                    <img src="/assets/2-MYT5zDHB.png" alt="placeholder"/>
+                </div>
+                <div className={style.c}>
+                    <img src="/assets/4-Bp-Zi30X.png" alt="placeholder"/>
+                </div>
+                <div className={style.d}>
+                    <img src="/assets/3-DogtsELa.png" alt="placeholder"/>
+                </div>
+            </div>
+            <Partners/>
 
-        </div>
+        </section>
     );
 };
 
--- src/utils/data.tsx
+++ src/utils/data.tsx
@@ -18,3 +18,17 @@ export const data2 : marquees_content[] = [
     { name: "sony2", image: '/assets/marquees/sony.png' },
     { name: "windows2", image: '/assets/marquees/windows.png' },
 ];
+
+export const data3 : marquees_content[]  = [
+    { name: 'allplayer', image: '/assets/allplayer.svg' },
+    { name: 'bobplayer', image: '/assets/bobplayer.svg' },
+
+    { name: 'hushplayer', image: '/assets/hushplayer.svg' },
+    { name: 'ibosol', image: '/assets/ibosol.svg' },
+    { name: 'iboss', image: '/assets/iboss.svg' },
+    { name: 'iboxx', image: '/assets/iboxx.svg' },
+    { name: 'kingiptv', image: '/assets/kingiptv.svg' },
+
+    { name: 'macplayer', image: '/assets/macplayer.svg' },
+    { name: 'virginia', image: '/assets/virginia.svg' },
+];
